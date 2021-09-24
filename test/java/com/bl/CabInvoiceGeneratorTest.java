0304b531a5822acdbd69773129bcc1395bfbd734
Daminichandrakar <damini.2201@gmail.com>
2021-09-24 18:50:05 +0530

    Added: Enhanced Invoice total fare calculation

--- test/java/com/bl/CabInvoiceGeneratorTest.java
+++ test/java/com/bl/CabInvoiceGeneratorTest.java
@@ -8,11 +8,12 @@ import org.junit.Test;
 
 public class CabInvoiceGeneratorTest {
 	CabInvoiceGenerator cabInvoiceGenerator = null;
+
 	@Before
-	public void setup() throws Exception{
+	public void setup() throws Exception {
 		cabInvoiceGenerator = new CabInvoiceGenerator();
 	}
-	
+
 	@Test
 	public void givenDistanceAndTime_ShouldReturnMaxFare() {
 		double result = cabInvoiceGenerator.calculateFare(2.0, 5);
@@ -26,18 +27,11 @@ public class CabInvoiceGeneratorTest {
 
 	}
 
-//	@Test
-//	public void givenMultipleDistanceAndTime_ShouldReturnTotalFare() {
-//		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
-//		double fare = cabInvoiceGenerator.calculateFare(rides);
-//		Assert.assertEquals(30, fare, 0.0);
-//	}
-	
 	@Test
 	public void givenMultipleDistanceAndTime_ShouldReturnInvoiceSummary() {
 		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
 		InnoiceSummary summary = cabInvoiceGenerator.calculateFare(rides);
-		InnoiceSummary expectedInnoiceSummary =	new InnoiceSummary(2,30.0);
-		Assert.assertEquals(expectedInnoiceSummary,summary);
+		InnoiceSummary expectedInnoiceSummary = new InnoiceSummary(2, 30.0);
+		Assert.assertEquals(expectedInnoiceSummary, summary);
 	}
 }
