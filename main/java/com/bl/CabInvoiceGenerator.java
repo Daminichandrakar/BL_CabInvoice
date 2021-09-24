9f5ee76931100034c6aebf519e3045cdf5c878c6
Daminichandrakar <damini.2201@gmail.com>
2021-09-24 16:34:04 +0530

    Added: calculate total fare

--- main/java/com/bl/CabInvoiceGenerator.java
+++ main/java/com/bl/CabInvoiceGenerator.java
@@ -1,9 +1,15 @@
 package com.bl;
 
 public class CabInvoiceGenerator {
+	 private final int MINIMUM_FARE = 5;
+	    private final int COST_PER_KILOMETER = 10;
+	    private final int COST_PER_MINUTE = 1;
 
-	public static void main(String[] args) {
-          System.out.println("welcome message");
-	}
-
+	    public double calculateFare(double distance, int time) {
+	        double totalFare = COST_PER_KILOMETER * distance + COST_PER_MINUTE * time;
+	        if (totalFare < MINIMUM_FARE) {
+	            return MINIMUM_FARE;
+	        }
+	        return totalFare;
+	    }	
 }
