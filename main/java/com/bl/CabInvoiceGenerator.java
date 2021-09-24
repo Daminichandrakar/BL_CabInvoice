1fa58f4b4c0cee38c13056c6786584afb5ccfa05
Daminichandrakar <damini.2201@gmail.com>
2021-09-24 17:31:15 +0530

    Added: calculate multiple rides total fare

--- main/java/com/bl/CabInvoiceGenerator.java
+++ main/java/com/bl/CabInvoiceGenerator.java
@@ -1,15 +1,20 @@
 package com.bl;
 
 public class CabInvoiceGenerator {
-	 private final int MINIMUM_FARE = 5;
-	    private final int COST_PER_KILOMETER = 10;
-	    private final int COST_PER_MINUTE = 1;
+	private final int MINIMUM_FARE = 5;
+	private final int COST_PER_KILOMETER = 10;
+	private final int COST_PER_MINUTE = 1;
 
-	    public double calculateFare(double distance, int time) {
-	        double totalFare = COST_PER_KILOMETER * distance + COST_PER_MINUTE * time;
-	        if (totalFare < MINIMUM_FARE) {
-	            return MINIMUM_FARE;
-	        }
-	        return totalFare;
-	    }	
+	public double calculateFare(double distance, int time) {
+		double totalFare = COST_PER_KILOMETER * distance + COST_PER_MINUTE * time;
+		return Math.max(totalFare,MINIMUM_FARE);
+	}
+
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
+	}
 }
